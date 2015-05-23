6a23343c034fce52c86762508956ecb7a719c43c
NovikovViktor <viktor.novikov95@gmail.com>
2015-05-23 16:21:28 +0300

    Add some simple sluggable functional

--- SluggableBehavior.php
+++ SluggableBehavior.php
@@ -2,4 +2,18 @@
 
 class SluggableBehavior extends CActiveRecordBehavior
 {
+    public $delimiter;
+
+    public $sluggable_attr;
+
+    public $slug_attr;
+
+    public function beforeSave()
+    {
+        $model = $this->getOwner();
+        $attr = $this->sluggable_attr;
+        $words = explode(' ', $model->$attr);
+        $slug = implode($this->delimiter, $words);
+        $model->slug = $slug;
+    }
 } 
